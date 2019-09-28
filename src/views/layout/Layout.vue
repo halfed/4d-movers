57f46dd1308ff63f55a38e13d1fb39cde6045f46
halfed <1half.ed@gmail.com>
2019-09-28 00:20:57 -0600

    refactoring side drawer and header

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,64 +1,58 @@
 <template>
 	<div class="grid-container fluid main">
-		
-
 		<div class="off-canvas-wrapper">
 	      <nav id="menu" class="grid-x grid-padding-x">
 	      	<div 
 	      		class="cell medium-12 slid-nav-container off-canvas-absolute position-left is-transition-push" 
 	      		v-bind:class="[slideout ? 'is-open' : 'is-closed']" 
 	      		id="offCanvasLeftSplit" 
-	      		data-off-canvas
 	      	>
-	      		<button class="close-toggle" @click="toggleSlidOutHandler">X</button>
+	      		<button class="close-toggle" @click="toggleHandler">X</button>
 		        <ul>
-	      			<li @click="toggleSlidOutHandler">
+	      			<li @click="toggleHandler">
 	      				<router-link 
 	      					v-bind:to="'/dev/services'"
-
 	      				>Service Areas</router-link>
 	      			</li>
-	      			<li @click="toggleSlidOutHandler">
+	      			<li @click="toggleHandler">
 	      				<router-link 
 	      				v-bind:to="'about-us'"
 	      			>About Us</router-link>
 	      			</li>
-	      			<li @click="toggleSlidOutHandler">
+	      			<li @click="toggleHandler">
 	      				<router-link 
 	      					v-bind:to="'request-quote'"
 	      				>Request A Quote</router-link>
 	      			</li>
 	      		</ul>
 		    </div>
-	        
 	      </nav>
-
-
 	      <main id="panel">
 	      	<div 
-	      		class="off-canvas-content"
+	      		class="off-canvas-content grid-x"
 	      		v-bind:class="[slideout ? 'is-open-left has-transition-push has-position-left' : '']"
-	      		data-off-canvas-content
 	      	>
-	      		<router-link v-bind:to="'/dev'" className="image-link">
-	      			<img src="../../assets/movers-logo.png">
-	      		</router-link>
-				<h1>4th DIMENSION PRO MOVERS</h1>
-				<h2>Apartments | Residence | Military</h2>
+	      		<div class="cell large-6 medium-12 small-12">
+	      			<router-link v-bind:to="'/dev'" className="image-link">
+	      				<img src="../../assets/movers-logo.png">
+	      			</router-link>
+	      		</div>
+	      		<div class="cell large-6 medium-12 small-12">
+	      			<div class="menu-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+				      	<button class="menu-icon toggle-button" type="button" @click="toggleHandler"></button>
+				      	<div class="title-bar-title">Menu</div>
+				          <span>850-427-8800</span>
+				    </div>
+	      			<h1>4th DIMENSION PRO MOVERS</h1>
+					<h2>Apartments | Residence | Military</h2>
+	      		</div>
 			</div>
-	        
-			<site-header
-				:toggleHandler="toggleSlidOutHandler"
-			/>
+			<site-header />
 			<router-view></router-view>
 			<footer-content/>
 	      </main>
-
-
-
 	    </div>
 	</div>
-
 </template>
 
 <script>
@@ -71,7 +65,7 @@
 			}
 	    },
 	    methods: {
-	      toggleSlidOutHandler() {
+	      toggleHandler() {
 	      	this.slideout = !this.slideout;
 	      }
 	    }
@@ -87,69 +81,43 @@
 		cursor: pointer;
 	}
 	.main {
+		.menu-bar {
+			display: block;
+			background-color: #bbc4db;
+			line-height: 2;
+    		color: #FFF;
+    		padding-left: 20px;
+
+    		span {
+    			float: right;
+    			padding-right: 20px;
+    			font-weight: 700;
+    			margin-top: 1px;
+    		}
+
+    		div {
+    			padding-left: 5px;
+    		}
+		}
 		h1 {
-		    position: relative;
-		    color: #FFF;
-		    font-weight: 700;
-		    font-size: 1.4rem;
-		    background-color: #000;
-		    padding-left: 26px;
-		    padding-top: 0px;
-		    padding-bottom: 1px;
-		    top: 40px;
-		    line-height: 2;
+			color: #FFF;
+			background-color: #000;
+			font-weight: 700;
+			font-size: 1.4rem;
+			line-height: 2;
+			text-align: center;
 		}
 
 		h2 {
-		    position: relative;
-		    padding-left: 20px;
-		    font-size: 1.3rem;
-		    font-weight: 500;
-    		top: 41px;
+			text-align: center;
+			font-size: 1.4rem;
 		}
-	}
-
-	@media only screen and (max-width: 320px) {
-	    .main {
-	    	h1 {
-	    		font-size: 1.4em;
-	    		padding-left: 15px;
-	    	}
-
-	    	h2 {
-			    font-size: 1.2em;
-			    padding-left: 23px;
-			}
-	    }
 	}
 
 	@media only screen and (max-width: 768px) and (min-width: 376px) {
 		.off-canvas-content {
 			background-image: url("../../assets/background-img.png");
 			background-repeat: repeat-x;
-		}
-
-		.main {
-			h1 {
-			    position: relative;
-			    color: #FFF;
-			    font-weight: 700;
-			    font-size: 2.0em;
-			    background-color: #000;
-			    padding-left: 26px;
-			    padding-top: 0px;
-			    padding-bottom: 1px;
-			    top: 40px;
-			    line-height: 2;
-			}
-
-			h2 {
-			    position: relative;
-			    padding-left: 20px;
-			    font-size: 1.5em;
-			    font-weight: 700;
-	    		top: 41px;
-			}
 		}
 	}
 
@@ -161,24 +129,27 @@
 		.main {
 			
 			img {
-				width: 34%;
+				width: 68%;
 				margin-top: 16px;
 			}
 
+			.menu-bar {
+				display: none;
+			}
+
 			h1 {
-				float: right;
-			    top: 41px;
-			    left: -19%;
+				text-align: left;
 			    color: #FFF;
 			    font-size: 2.2rem;
 			    background-color: transparent;
+			    padding-top: 4%;
 			}
 
 			h2 {
-				float: right;
-			    top: -64px;
-			    left: -20%;
+				text-align: left;
+			    top: 25%;
 			    font-size: 2.0rem;
+			    position: relative;
 			}
 		}
 	}
@@ -186,17 +157,7 @@
 	@media only screen and (width: 1024px) {
     	.main {
 			img {
-				width: 40%;
-			}
-
-			h1 {
-			    top: 30px;
-			    left: -9%;
-			}
-
-			h2 {
-			    top: -60px;
-			    left: -9%;
+				width: 80%;
 			}
 		}
   	}
