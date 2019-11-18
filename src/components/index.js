930dd91ed8ea869d840426036bc6366b6de71584
halfed <1half.ed@gmail.com>
2019-11-17 19:06:48 -0700

    renaming site header to top nav

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import HelloWorld from './HelloWorld';
-import SiteHeader from './SiteHeader/SiteHeader';
+import TopNav from './TopNav/TopNav';
 import TestSlot from './TestSlot';
 import FooterContent from './footer/Footer';
 import PageBody from './PageBody/PageBody';
 
 export default [
 	HelloWorld,
-	SiteHeader,
+	TopNav,
 	TestSlot,
 	PageBody,
 	FooterContent
